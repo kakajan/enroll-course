cbf16fd388e2bba6280c281dcd735e0a69d0a964
UsherQelich SayyedMuhammadi <aykam-co@hotmail.com>
2023-02-07 22:24:32 +0330

    registeration form setup with some design

--- FE/src/App.vue
+++ FE/src/App.vue
@@ -19,13 +19,20 @@ export default defineComponent({
 }
 .r35 {
   border-radius: 35px;
+  overflow: hidden!important;
+}
+.r56 {
+  border-radius: 56px;
+  overflow: hidden!important;
 }
 .bg-glass {
   background: rgba(255, 255, 255, 0.21);
-  border-radius: 16px;
   box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
   backdrop-filter: blur(5.1px);
   -webkit-backdrop-filter: blur(5.1px);
   border: 1px solid rgba(255, 255, 255, 0.3);
+}
+.q-field__label, .q-field--float .q-field__label{
+  color: white
 }
 </style>
--- FE/src/pages/IndexPage.vue
+++ FE/src/pages/IndexPage.vue
@@ -7,23 +7,33 @@
             <q-item-section avatar>
               <img src="signup.png" />
             </q-item-section>
-            <q-item-label class="text-h5 text-weight-bold morabba text-indigo-10 q-pt-sm">
+            <q-item-label class="text-h5 text-weight-bold morabba text-white q-pt-sm">
               ثبت نام مهمان فول استک <span class="text-h4 text-black text-weight-bolder">11</span>
             </q-item-label>
           </q-item>
+          <q-item>
+            <q-item-section class="q-gutter-y-md">
+              <q-input v-model="fullName" input-class="text-black" color="indigo-10" class="bg-glass r56" rounded outlined label="نام و نام خانوادگی" />
+              <q-input maxlength="10" input-class="text-black" dir="ltr" suffix="98+" type="tel" color="indigo-10" class="bg-glass r56" rounded outlined label="شماره همراه" />
+              <q-btn unelevated color="purple-8" label="ثبت نام" icon-right="rocket_launch" rounded size="lg" />
+            </q-item-section>
+          </q-item>
         </q-list>
-      </q-card-section>
-      <q-card-section>
-        <q-input label="نام و نام خانوادگی" />
       </q-card-section>
     </q-card>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 
 export default defineComponent({
   name: "IndexPage",
+  setup () {
+    const fullName = ref(null)
+    return {
+      fullName
+    }
+  }
 });
 </script>
