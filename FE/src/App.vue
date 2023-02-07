c6af801b7bc34a4ba0ec561460a7319031eabcde
Usher <faslolkhitab@gmail.com>
2023-02-07 18:02:50 +0330

    Persian fonts added. The license is required

--- FE/src/App.vue
+++ FE/src/App.vue
@@ -3,9 +3,29 @@
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'App'
-})
+  name: "App",
+});
 </script>
+<style>
+.bg {
+  background: url("bg.webp") no-repeat center top;
+  background-size: cover;
+}
+.morabba {
+  font-family: "Morabba" !important;
+}
+.r35 {
+  border-radius: 35px;
+}
+.bg-glass {
+  background: rgba(255, 255, 255, 0.21);
+  border-radius: 16px;
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(5.1px);
+  -webkit-backdrop-filter: blur(5.1px);
+  border: 1px solid rgba(255, 255, 255, 0.3);
+}
+</style>
--- FE/src/pages/IndexPage.vue
+++ FE/src/pages/IndexPage.vue
@@ -1,17 +1,26 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page class="flex flex-center bg">
+    <q-card class="r35 bg-glass">
+      <q-card-section>
+        <q-list>
+          <q-item>
+            <q-item-section avatar>
+              <q-icon name="person" />
+            </q-item-section>
+            <q-item-label class="text-h5 text-weight-bolder morabba text-white">
+              فرم ثبت نام در جلسه اول
+            </q-item-label>
+          </q-item>
+        </q-list>
+      </q-card-section>
+    </q-card>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'IndexPage'
-})
+  name: "IndexPage",
+});
 </script>
