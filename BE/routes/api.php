e32650ecf5b428c2cf77b2a5fd18b958e853b6d8
UsherQelich SayyedMuhammadi <aykam-co@hotmail.com>
2023-02-09 00:01:49 +0330

    test ui action

--- BE/routes/api.php
+++ BE/routes/api.php
@@ -17,4 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('reg', function () {});
+Route::post('reg', function () {
+    return 1;
+});
